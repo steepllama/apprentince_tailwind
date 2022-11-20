512623acce43d808573453b141e7ae301f935627
steepllama <steepllama@gmail.com>
2022-11-19 18:40:21 -0600

    started the basics for the home page

---
+++ src/Home/index.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const index = () => {
+  return (
+    <div>
+      <div className="container">
+        <h2 className="about-container">
+          Our Mission
+        </h2>
+      </div>
+
+      <div className="mission-container">
+        <section className="para">
+          <p className="description">The lifelong mission of Onesimo Electric is to provide 100% customer satisfaction to all of our guests. Our electricians are highly trained and focused to ensure that you receive the product that you requested. We strive to provide electricity for future homes and to existing homes.</p>
+        </section>
+      </div>
+    </div>
+  )
+}
+
+export default index;
